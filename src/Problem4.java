57da070eff2e1e8524f067400c2dc2f8722bf14c
Stammnitz <stammnitz@win.embedded.rwth-aachen.de>
2025-05-21 15:59:33 +0200

    Problem 4-2

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+
 public class Problem4 {
 
     public static void main(String[] args) {
+        ArrayList<Integer> palindroms = getPalindromes(998001);
+
+
+        for (int i = palindroms.size() - 1; i >= 0; i--) {
+            System.out.println(palindroms.get(i));
+        }
 
     }
 
@@ -8,14 +16,22 @@ public class Problem4 {
 
         int original = number;
         int numberReversed = 0;
-
         while (number != 0) {
             int letzteZiffer = number % 10;
             numberReversed = numberReversed * 10 + letzteZiffer;
             number /= 10;
         }
-
         return original == numberReversed;
+    }
+
+    public static ArrayList<Integer> getPalindromes(int range){
+        ArrayList<Integer> palindroms = new ArrayList<Integer>();
+        for(int i = 1; i <= range; i++) {
+            if(isPalindrome(i)) {
+                palindroms.add(i);
+            }
+        }
+        return palindroms;
     }
 
 }
